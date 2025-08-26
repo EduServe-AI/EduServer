35f05e0b4a58fdb078d43f3448516c0e00bbb112
Hasan <hassandemon12@gmail.com>
2025-08-26 13:15:28 +0530

    Fixed circular dependency issue

--- src/config/database.js
+++ src/config/database.js
@@ -13,7 +13,7 @@ module.exports = {
         dialectOptions: {
             ssl: {
                 require: true,
-                rejectUnauthorized: false,
+                rejectUnauthorized: true,
             },
         },
     },
@@ -24,7 +24,7 @@ module.exports = {
         dialectOptions: {
             ssl: {
                 require: true,
-                rejectUnauthorized: false,
+                rejectUnauthorized: true,
             },
         },
     },
